630578a4fb303504a5973ba39bef9add4c8ef054
wintyo <wintyo@users.noreply.github.com>
2023-03-11 08:59:51 +0000

    deploy: b5a6aae38da2dd12105a7c92ebbb7a473b083c71

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * 加算メソッド
+ * @param x - 数値
+ * @param y - 数値
+ * @returns - 足した値
+ */
+export declare const add: (x: number, y: number) => number;
---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+/**
+ * 加算メソッド
+ * @param x - 数値
+ * @param y - 数値
+ * @returns - 足した値
+ */
+const add = (x, y) => {
+    return x + y;
+};
+exports.add = add;
+//# sourceMappingURL=index.js.map
